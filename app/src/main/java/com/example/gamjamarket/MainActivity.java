4c9563477478906f3f08738a83890e77962574c7
kinggora <jhha09@ajou.ac.kr>
2021-10-03 18:29:56 +0900

    gamja-market

---
+++ app/src/main/java/com/example/gamjamarket/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.gamjamarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}

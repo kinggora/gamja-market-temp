6c4d6983b4d93d692c48402e8c5869f6d1cbd390
kimhyojin97 <83204911+kimhyojin97@users.noreply.github.com>
2021-12-13 22:47:26 +0900

    판매자프로필

---
+++ app/src/main/java/com/example/gamjamarket/Info/ProfileActivity.java
@@ -0,0 +1,75 @@
+package com.example.gamjamarket.Info;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gamjamarket.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class ProfileActivity extends AppCompatActivity {
+    private static final String TAG = "InfoActivity";
+
+    private TextView nickname;
+    private LinearLayout product;
+    private LinearLayout review;
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        mAuth = FirebaseAuth.getInstance();
+        String uid = getIntent().getStringExtra("uid");
+        nickname = (TextView) findViewById(R.id.profileActivity_nickname);
+        product = (LinearLayout)findViewById(R.id.profileActivity_product);
+        review = (LinearLayout)findViewById(R.id.profileActivity_review);
+
+        DocumentReference docRef = db.collection("users").document(uid);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        nickname.setText(document.getString("nickname"));
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+        product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, InfoModifyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        review.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, ReviewFragment.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/gamjamarket/Model/UserModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.gamjamarket.Model;
-
-public class UserModel {
-    public String username;
-    public String profileImageUrl;
-    public String uid;
-}
